22234b881c781171052656a7636b63a6d4015c40
irenasestak <sestakirena@gmail.com>
2021-06-06 16:33:16 +0200

    completed pages

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
-//AOS.init();
-$(document).ready(function() {
 
+$(document).ready(function () {
+	AOS.init();
 	const menu = $('.menu');
 	const navList = $('.nav-list');
 	const filter = $('.filter');
@@ -10,12 +10,12 @@ $(document).ready(function() {
 	$(window).on('resize', mediaLinks);
 
 	// show/hide menu on scroll
-	$(window).on('scroll', function() {
+	$(window).on('scroll', function () {
 		$('header').toggleClass('d-none', $(window).scrollTop() > previousScrollPosition);
 		previousScrollPosition = $(window).scrollTop();
 	});
 
-	menu.on('click', function() {
+	menu.on('click', function () {
 		if (!showMenu) menuShow();
 		else menuHide();
 	});
@@ -25,7 +25,7 @@ $(document).ready(function() {
 		navList.addClass('d-flex');
 		filter.removeClass('d-none');
 		filter.addClass('d-flex;');
-		$( '.line' ).addClass('transform-menu');
+		$('.line').addClass('transform-menu');
 		showMenu = true;
 	}
 
@@ -34,10 +34,10 @@ $(document).ready(function() {
 		navList.addClass('d-none');
 		filter.removeClass('d-flex');
 		filter.addClass('d-none');
-		$( '.line' ).removeClass('transform-menu');
+		$('.line').removeClass('transform-menu');
 		showMenu = false;
 	}
- 
+
 	// show/hide on load & resize
 	function mediaLinks() {
 		if (window.innerWidth > 767) {
@@ -51,4 +51,57 @@ $(document).ready(function() {
 			$('.nav-list li a').on('click', menuHide);
 		}
 	}
+
+	//  back to top button
+	$(window).on('scroll', function () {
+		if ($(this).scrollTop() > 400) {
+			$('.back-to-top').css('display', 'block');
+		} else {
+			$('.back-to-top').css('display', 'none');
+		}
+	});
+
+	$('.back-to-top').on('click', function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 400);
+	});
+
+	// Magnific Popup plugin - init form open
+	$('#form-link').magnificPopup({
+		items: {
+			src: '#popup',
+			type: 'inline'
+		}
+	});
+
+	// tabs
+	$('.tab-content').hide();
+	$('ul.tabs li:first').addClass('active').show();
+	$('.tab-content:first').show();
+
+	$('ul.tabs li').click(function () {
+		$('ul.tabs li').removeClass('active');
+		$(this).addClass('active');
+		$('.tab-content').hide();
+		let activeTab = $(this).find('a').attr('href');
+		$(activeTab).fadeIn();
+		return false;
+	});
+
+	// lazy loading bg images (loading when entering the viewport)
+	const lazyBackground = document.querySelectorAll('.lazy-bg');
+	console.log(lazyBackground)
+	const bgImgObserver = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (!entry.isIntersecting) {
+				return;
+			}
+			console.log('bg enetering!')
+			entry.target.classList.remove('no-bg');
+			bgImgObserver.unobserve(entry.target);
+		});
+	}, {});
+	lazyBackground.forEach((bgImg) => bgImgObserver.observe(bgImg));
 });
+
